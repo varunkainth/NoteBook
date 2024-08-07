283e79a2a130708e326d24d773ac519f2b4363e0
swabhi16s <145577315+swabhi16s@users.noreply.github.com>
2024-08-07 14:26:06 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import App from './App';
+import Registration from './components/Registration';
+import { ThemeProvider } from './ThemeContext';
+import SideNavbar from './components/SideNavbar';
+import UpdateProfile from './components/UpdateProfile';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <ThemeProvider>
+    
+    <Router>
+    <SideNavbar/>
+    
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/Register" element={<Registration />} />
+        <Route path="/update-profile" element={<UpdateProfile />} />
+      </Routes>
+    </Router>
+  </ThemeProvider>
+);
+
